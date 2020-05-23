5be55c5e07a542d8d5df751638e16bb59cb83cf4
ivantelix <ivantelix@gmail.com>
2020-05-23 09:02:37 -0400

    [ADD]Add job file for rate consult

---
+++ app/Jobs/RateConsult.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+use App\Model\Payment;
+
+class RateConsult implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $payment;
+
+    public function __construct(Payment $payment)
+    {
+        $this->payment = $payment;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $date = \Carbon::now();
+        $payment = Payment::where('crated_at', '=', $date)
+                                ->where('clp_usd', '!=', '')
+                                ->first();
+
+        if(!$payment) {
+            
+        }
+    }
+}
